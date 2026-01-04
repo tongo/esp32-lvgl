8af98c1a9d91a19617e26f12958692f5795d3049
Leonardo Tonghini <l.tonghini@tangodev.it>
2026-01-04 20:54:32 +0100

    Aggiunto spinner splash

--- esp32-lvgl/main/main.cpp
+++ esp32-lvgl/main/main.cpp
@@ -235,18 +235,28 @@ extern "C" void app_main(void) {
         lv_obj_set_style_bg_opa(labelRed, LV_OPA_TRANSP, 0);
         lv_obj_set_style_text_color(labelRed, UiTheme::accentRedColor, 0);
         lv_obj_set_style_text_font(labelRed, UiTheme::fontH2, 0);
-        lv_obj_align(labelRed, LV_ALIGN_CENTER, 2, 1);
+        lv_obj_align(labelRed, LV_ALIGN_CENTER, 2, -20);
 
         lv_obj_t *labelWhite = lv_label_create(lv_screen_active());
         lv_label_set_text(labelWhite, "TANGODEV");
         lv_obj_set_style_bg_opa(labelWhite, LV_OPA_TRANSP, 0);
         lv_obj_set_style_text_font(labelWhite, UiTheme::fontH2, 0);
-        lv_obj_center(labelWhite);
+        lv_obj_align(labelWhite, LV_ALIGN_CENTER, 0, -21);
 
         lv_obj_t *subtitle = lv_label_create(lv_screen_active());
         lv_label_set_text(subtitle, "DATA\nACQUISITION");
-        lv_obj_align(subtitle, LV_ALIGN_BOTTOM_MID, 0, -60);
+        lv_obj_align(subtitle, LV_ALIGN_CENTER, 0, 20);
         lv_obj_set_style_text_align(subtitle, LV_TEXT_ALIGN_CENTER, 0);
+
+        lv_obj_t * spinner = lv_spinner_create(lv_screen_active());
+        lv_obj_set_size(spinner, 30, 30);
+        lv_obj_align(spinner, LV_ALIGN_BOTTOM_MID, 0, -12);
+        lv_spinner_set_anim_params(spinner, 3000, 330);
+        lv_obj_set_style_arc_color(spinner, UiTheme::bgColor, LV_PART_MAIN);
+        lv_obj_set_style_arc_width(spinner, 3, LV_PART_MAIN);
+        lv_obj_set_style_arc_color(spinner, UiTheme::textColor, LV_PART_INDICATOR);
+        lv_obj_set_style_arc_width(spinner, 3, LV_PART_INDICATOR);
+        lv_obj_set_style_arc_rounded(spinner, true, LV_PART_INDICATOR);
 
         /*
         lv_obj_t * redCircle = lv_obj_create(lv_screen_active());
